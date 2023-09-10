58f182fe6743d9d9df9a801f43c2b0f648385aab
hassam sohail <62680104+hassamsohail@users.noreply.github.com>
2023-09-10 07:32:40 +0500

    ok

--- screen/Subscription.js
+++ screen/Subscription.js
@@ -41,6 +41,7 @@ export default function Subscription({navigation}) {
   //   requestLocationPermission(); 
   // }, []);
 
+  
   // const getCurrentLocation = () => {
   //   Geolocation.getCurrentPosition(
   //     (position) => {
